a1dfaf15e766701fb07eb4e3feefa24afe60f4a0
Roy Li <ruyili2002@gmail.com>
2018-11-17 15:12:30 -0500

    support ancient browsers

--- console.image.js
+++ console.image.js
@@ -1,9 +1,10 @@
-console.image = (url) => {
-    const img = new Image();
+console.image = function (url) {
+    var img = new Image();
     img.src = url;
-    img.onload = () => {
-        const { width, height } = img;
-        console.log('%c ', `font-size:0;background-image:url('${url}');padding:${width/2}px ${height/2}px;`);
-    }
+    img.onload = function () {
+        var width = img.width,
+            height = img.height;
+        console.log('%c ', 'font-size:0;background-image:url(\'' + url + '\');padding:' + width / 2 + 'px ' + height / 2 + 'px;');
+    };
     img.onerror = console.error;
-}
+};
